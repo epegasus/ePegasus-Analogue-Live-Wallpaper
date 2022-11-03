f8ad752bf7e4536d18ae7c5c85def0af9f5d3d22
Sohaib <sohaibahmed.hypersoft@gmail.com>
2022-11-03 16:37:13 +0500

    Initial Commit

--- app/src/main/java/dev/epegasus/analogueservice/service/PegasusWallpaperService.kt
+++ app/src/main/java/dev/epegasus/analogueservice/service/PegasusWallpaperService.kt
@@ -1,13 +1,19 @@
 package dev.epegasus.analogueservice.service
 
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.PorterDuff
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.service.wallpaper.WallpaperService
 import android.util.Log
 import android.view.SurfaceHolder
+import androidx.annotation.DrawableRes
+import dev.epegasus.analogueservice.R
+import java.util.Calendar
 
 class PegasusWallpaperService : WallpaperService() {
 
@@ -33,12 +39,19 @@ class PegasusWallpaperService : WallpaperService() {
 
         private val handler by lazy { Handler(Looper.getMainLooper()) }
         private val runnable by lazy { Runnable { draw() } }
-        private val paint by lazy {
-            Paint().apply {
-                color = Color.WHITE
-                strokeWidth = 5f
-            }
-        }
+
+        private val bitmapBg by lazy { getBMP(R.drawable.img_clock_bg) }
+        private val bitmapHr by lazy { getBMP(R.drawable.img_clock_hr) }
+        private val bitmapMin by lazy { getBMP(R.drawable.img_clock_min) }
+        private val bitmapSec by lazy { getBMP(R.drawable.img_clock_sec) }
+
+        private var isDefaultUpdated = false
+        private var imageResolution = 200
+        private var xCoordinateCanvas = 100f
+        private var yCoordinateCanvas = 300f
+        private var degreeHr = 0.0f
+        private var degreeMin = 0.0f
+        private var degreeSec = 0.0f
 
         override fun onCreate(surfaceHolder: SurfaceHolder?) {
             super.onCreate(surfaceHolder)
@@ -48,34 +61,38 @@ class PegasusWallpaperService : WallpaperService() {
             setOffsetNotificationsEnabled(false)
         }
 
-        override fun onSurfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
-            super.onSurfaceChanged(holder, format, width, height)
-            Log.d(TAG, "onSurfaceChanged: called")
-        }
-
-        override fun onSurfaceCreated(holder: SurfaceHolder?) {
-            super.onSurfaceCreated(holder)
-            Log.d(TAG, "onSurfaceCreated: called")
-        }
-
-        override fun onSurfaceDestroyed(holder: SurfaceHolder?) {
-            super.onSurfaceDestroyed(holder)
-            Log.d(TAG, "onSurfaceDestroyed: called")
-        }
-
         /**
-         *  It is very important that a wallpaper only use CPU while it is visible.
+         *  It is very important that a wallpaper only use CPU while it is visible. So, only draw if visible.
          */
         override fun onVisibilityChanged(visible: Boolean) {
             super.onVisibilityChanged(visible)
             // Start/Stop drawing on Canvas
+            getInitialAllAngle()
             if (visible) handler.post(runnable)
             else handler.removeCallbacks(runnable)
+        }
+
+        /**
+         * Set angles of all needles according to current time
+         *      Hours:      total 12h (Rotation: 360)
+         *      Minutes:    total 60m (Rotation: 360) ~ 360/60 = 6 times
+         *      Seconds:    total 60s (Rotation: 360) ~ 360/60 = 6 times
+         */
+        private fun getInitialAllAngle() {
+            val calender = Calendar.getInstance()
+            val hour = calender.get(Calendar.HOUR)
+            val minute = calender.get(Calendar.MINUTE)
+            val second = calender.get(Calendar.SECOND)
+
+            degreeHr = (hour * 30F) + (minute * 0.5F)
+            degreeMin = minute * 6F
+            degreeSec = second * 6F
         }
 
         private fun draw() {
             // if 'Surface' is not created yet! lockCanvas cannot provide canvas for performing any action
             if (!surfaceHolder.surface.isValid) {
+                Log.d(TAG, "draw: Surface is not Valid (created yet).")
                 return
             }
             val canvas = try {
@@ -85,13 +102,101 @@ class PegasusWallpaperService : WallpaperService() {
                     surfaceHolder.lockCanvas()
                 }
             } catch (ex: Exception) {
-                Log.d(TAG, "draw: $ex")
+                Log.d(TAG, "draw: LockHardwareCanvas: $ex")
                 surfaceHolder.lockCanvas()
             }
-            val startPair = Pair((canvas.width / 2).toFloat(), 0f)
-            val endPair = Pair((canvas.width / 2).toFloat(),  (canvas.height / 1).toFloat())
-            canvas.drawLine(startPair.first, startPair.second, endPair.first, endPair.second, paint)
-            surfaceHolder.unlockCanvasAndPost(canvas)
+
+            canvas?.let {
+                // Update image 'height/width' only if not default
+                if (!isDefaultUpdated) {
+                    isDefaultUpdated = true
+                    imageResolution = (it.width * 70) / 100
+                    xCoordinateCanvas = ((it.width / 2) - imageResolution / 2).toFloat()
+                    yCoordinateCanvas = ((it.height / 2) - imageResolution / 2).toFloat()
+                }
+                // Set Background of Clock
+                it.drawColor(0, PorterDuff.Mode.CLEAR)
+                it.drawBitmap(bitmapBg, xCoordinateCanvas, yCoordinateCanvas, null)
+
+                hrNeedleControl(it)
+                minNeedleControl(it)
+                secNeedleControl(it)
+                surfaceHolder.unlockCanvasAndPost(it)
+                repeatMethod()
+            } ?: kotlin.run {
+                Log.d(TAG, "draw: Canvas is not found.")
+                return
+            }
+        }
+
+        private fun repeatMethod() {
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable, 1000)
+        }
+
+        private fun getBMP(@DrawableRes resId: Int): Bitmap {
+            val temp = BitmapFactory.decodeResource(applicationContext.resources, resId)
+            return Bitmap.createScaledBitmap(temp, imageResolution, imageResolution, true)
+        }
+
+        private fun hrNeedleControl(canvas: Canvas) {
+            val scaleWidth = (imageResolution / bitmapHr.width).toFloat()
+            val scaleHeight = (imageResolution / bitmapHr.height).toFloat()
+            val matrix = Matrix().apply {
+                postScale(scaleWidth, scaleHeight)
+                postRotate(degreeHr)
+            }
+            val bitmap = Bitmap.createBitmap(bitmapHr, 0, 0, bitmapHr.width, bitmapHr.height, matrix, true)
+
+            val leftCoordinate = ((canvas.width - bitmap.width) / 2).toFloat()
+            val topCoordinate = ((canvas.height - bitmap.height) / 2).toFloat()
+            canvas.drawBitmap(bitmap, leftCoordinate, topCoordinate, null)
+        }
+
+        private fun minNeedleControl(canvas: Canvas) {
+            val scaleWidth = (imageResolution / bitmapMin.width).toFloat()
+            val scaleHeight = (imageResolution / bitmapMin.height).toFloat()
+            val matrix = Matrix().apply {
+                postScale(scaleWidth, scaleHeight)
+                postRotate(degreeMin)
+            }
+            val bitmap = Bitmap.createBitmap(bitmapMin, 0, 0, bitmapMin.width, bitmapMin.height, matrix, true)
+
+            val leftCoordinate = ((canvas.width - bitmap.width) / 2).toFloat()
+            val topCoordinate = ((canvas.height - bitmap.height) / 2).toFloat()
+            canvas.drawBitmap(bitmap, leftCoordinate, topCoordinate, null)
+        }
+
+        private fun secNeedleControl(canvas: Canvas) {
+            val scaleWidth = (imageResolution / bitmapSec.width).toFloat()
+            val scaleHeight = (imageResolution / bitmapSec.height).toFloat()
+            val matrix = Matrix()
+            matrix.postScale(scaleWidth, scaleHeight)
+            matrix.postRotate(degreeSec)
+
+            val bitmap = Bitmap.createBitmap(bitmapSec, 0, 0, bitmapSec.width, bitmapSec.height, matrix, false)
+            updateAngles()
+
+            val leftCoordinate = ((canvas.width - bitmap.width) / 2).toFloat()
+            val topCoordinate = ((canvas.height - bitmap.height) / 2).toFloat()
+            canvas.drawBitmap(bitmap, leftCoordinate, topCoordinate, null)
+        }
+
+        private fun updateAngles() {
+            if (degreeSec >= 360) {
+                degreeSec = 6.0F
+                if (degreeMin >= 360) {
+                    degreeMin = 6.0F
+                    if (degreeHr >= 360) degreeHr = 0.5F else degreeHr += 0.5F
+                } else {
+                    degreeMin += 6.0F
+                    if (degreeMin % 6 == 0.0F) {
+                        if (degreeHr >= 360) degreeHr = 0.5F else degreeHr += 0.5F
+                    }
+                }
+            } else {
+                degreeSec += 6.0F
+            }
         }
     }
 
